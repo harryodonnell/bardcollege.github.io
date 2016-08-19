3e97aedd4e78229202691628b810e4c467c9c31f
harryodonnell <po7033@bard.edu>
2016-08-19 10:22:21 -0400

    Update eliza.js

--- workshop1/eliza.js
+++ workshop1/eliza.js
@@ -29,7 +29,7 @@
 
 var rules = {
   "~*x hello ~*y": [
-    "How do you do. Please state your problem."
+    "What the fuck is up????"
   ],
   "~*x computer ~*y": [
     "Do computers worry you?",
